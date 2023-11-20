04112b6364eb1c1adf140bfdab22a67ca68d3ac3
Danyl Brahin <inicerer141@gmail.com>
2023-11-20 19:04:01 +0200

    Remove flipper

--- android/app/src/main/java/com/notizpad/MainApplication.java
+++ android/app/src/main/java/com/notizpad/MainApplication.java
@@ -7,11 +7,6 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
-import com.facebook.flipper.android.AndroidFlipperClient;
-import com.facebook.flipper.android.utils.FlipperUtils;
-import com.facebook.flipper.core.FlipperClient;
-import com.facebook.flipper.plugins.inspector.DescriptorMapping;
-import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
 
@@ -58,11 +53,5 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, false);
-
-    if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
-      final FlipperClient client = AndroidFlipperClient.getInstance(this);
-      client.addPlugin(new InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()));
-      client.start();
-    }
   }
 }
