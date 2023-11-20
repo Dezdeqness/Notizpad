5ba94b76f366f2d8fa8a5bc99c839ffed9a9c32c
Danyl Brahin <inicerer141@gmail.com>
2023-11-20 19:07:17 +0200

    Add icons resources

---
+++ assets/index.ts
@@ -0,0 +1,6 @@
+import Delete from './icons/ic_delete.svg';
+import Edit from './icons/ic_edit.svg';
+import Location from './icons/ic_location.svg';
+import PhotoPlaceholder from './icons/ic_photo_placeholder.svg';
+
+export { Delete, Edit, Location, PhotoPlaceholder };
